572c35922411dfacead00dc6f9371f824fd25ed5
CruelMist <jokerj114@gmail.com>
2018-08-28 23:44:51 +0530

    Assignment 6

---
+++ Object_Oriented_Programming/Animal_Info.java
@@ -0,0 +1,36 @@
+class Animal
+{
+    String breed,color;
+    void Speak()
+    {
+
+    }
+}
+class Dog extends Animal
+{
+    void Speak()
+    {
+        breed="Husky";
+        color="Black";
+    }
+}
+class Cat extends Animal
+{
+    void Speak()
+    {
+        breed="Khao Manee";
+        color="White";
+    }
+}
+class Animal_Info
+{
+    public static void main(String[] args)
+    {
+        Dog ob1=new Dog();
+        Cat knobi=new Cat();
+        ob1.Speak();
+        System.out.println("I own a dog named Kuro. He is a "+ob1.breed+" and is "+ob1.color+" in color." );
+        knobi.Speak();
+        System.out.println("I also own a cat named Shiro. She is a "+knobi.breed+" and is "+knobi.color+" in color." );
+    }
+}
